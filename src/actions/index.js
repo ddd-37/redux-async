b11fca28abf367bd62bd17d0b83bba4bfcacf97f
Devon Deason <devon.d.deason@gmail.com>
2020-03-31 17:30:09 -0600

    fix overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("about to fethc posts");
+  await dispatch(fetchPosts());
+
+  let userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => {
+    dispatch(fetchUser(id));
+  });
+};
 
 // This looks odd, but just remeber that these functions are returning functions
 export const fetchPosts = () => async dispatch => {
@@ -13,3 +24,12 @@ export const fetchUser = userId => async dispatch => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// Ref to memoize
+// export const fetchUser = userId => dispatch => _fetchUser(userId, dispatch);
+
+// const _fetchUser = _.memoize(async (userId, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,21 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "./../actions";
 
-class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
+const UserHeader = props => {
+  const { user } = props;
+  if (!user) {
+    return null;
   }
 
-  render() {
-    const { user } = this.props;
-    if (!user) {
-      return null;
-    }
-
-    return <div className="header">{user.name}</div>;
-  }
-}
+  return <div className="header">{user.name}</div>;
+};
 
 // If we need to precalculate any data, we can use the ownProps param to find the particular props of this specific component
 const mapStateToProps = (state, ownProps) => {
@@ -24,4 +17,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
